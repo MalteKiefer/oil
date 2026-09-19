6f0db8f62f952283fc5a62cdf883d187f7f8b87e
Malte Kiefer <malte.kiefer@mailgermania.de>
2026-09-19 09:31:00 +0200

    feat: wire main entry point with signal handling and exit codes

--- cmd/verbrauch/main.go
+++ cmd/verbrauch/main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"errors"
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/maltekiefer/verbrauch/internal/cli"
+)
 
 func main() {
-	fmt.Println("verbrauch: CLI zur Erfassung von Zählerständen (Öl, Strom, ...)")
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
+	defer stop()
+
+	root := cli.NewRootCmd()
+
+	if err := root.ExecuteContext(ctx); err != nil {
+		fmt.Fprintln(os.Stderr, "Fehler:", err)
+
+		var usageErr *cli.UsageError
+		if errors.As(err, &usageErr) {
+			os.Exit(2)
+		}
+		os.Exit(1)
+	}
 }
